4e5ae11114e210a585ddcb012b43361a56f4d2cf
Wei Chen <smile.v.chen@gmail.com>
2023-06-24 22:17:33 +0900

    bundle exec rails db:migrate

--- db/migrate/20230624130902_create_users.rb
+++ db/migrate/20230624130902_create_users.rb
@@ -1,12 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
-  def change
-    create_table :users do |t|
-      t.string :login_id
-      t.string :password_digest
+  def up
+    create_table :users, id: :bigint, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+      t.string :login_id, null: false
+      t.string :password_digest, null: false
       t.string :nickname
 
       t.timestamps
     end
     add_index :users, :login_id, unique: true
+  end
+
+  def down
+    drop_table :users, if_exists: true
   end
 end
