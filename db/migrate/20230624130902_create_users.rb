747e4df5043047d4e9111d2dd21acb55f412840e
Wei Chen <smile.v.chen@gmail.com>
2023-06-24 22:09:47 +0900

    bundle exec rails g model User login_id:string:uniq password_digest:string nickname:string

---
+++ db/migrate/20230624130902_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :login_id
+      t.string :password_digest
+      t.string :nickname
+
+      t.timestamps
+    end
+    add_index :users, :login_id, unique: true
+  end
+end
